0d2e1e5415b475bfc8e5a46c502d9c9b55e7469b
BoHan <han.bo1@husky.neu.edu>
2018-02-03 16:53:53 -0500

    KSum

---
+++ Ksum.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ksum {
+    private List<List<Integer>> kSum(int kVal, int target, int startIndex, int[] nums )
+    {
+        List<List<Integer>> result = new LinkedList<>();
+        if ( kVal == 0 )
+        {
+            if ( target == 0 )
+            {
+                result.add( new LinkedList<>() );
+            }
+            return result;
+        }
+
+        for ( int i = startIndex; i < nums.length - kVal + 1; i++ )
+        {
+            if ( ( i > startIndex ) && ( nums[i] == nums[i - 1] ) )
+            {
+                continue;
+            }
+            for ( List<Integer> partialResult : kSum( kVal - 1, target - nums[i], i + 1, nums ) )
+            {
+                partialResult.add( 0, nums[i] );
+                result.add( partialResult );
+            }
+        }
+        return result;
+    }
+}
