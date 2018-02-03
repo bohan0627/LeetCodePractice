ab86235da18e7260b61824613820714da297f771
BoHan <han.bo1@husky.neu.edu>
2018-02-03 17:47:32 -0500

    Valid Parentheses.java

---
+++ Valid Parentheses.java
@@ -0,0 +1,42 @@
+import sun.jvm.hotspot.tools.SysPropsDumper;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class VParentheses {
+
+
+    public static boolean valid(String s)
+    {
+        if(s==null||s.length()==0)
+            return false;
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put('{','}');
+        map.put('(',')');
+        map.put('[',']');
+
+        int i = 0;
+        char c = s.charAt(i);
+        while(map.keySet().contains(c) && i<s.length()-1 && map.get(c) == s.charAt(i+1)  )
+            {
+                if(i == s.length()-2)
+                return true;
+            i += 2;
+            c = s.charAt(i);
+        }
+        return false;
+
+    }
+    
+
+
+    public static void main(String[] args)
+    {
+        String s = " ";
+        boolean status;
+        status = VParentheses.valid(s);
+        System.out.println(status);
+
+
+    }
+}
