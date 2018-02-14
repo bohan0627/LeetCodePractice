49b2eb2204709b463f75f2a4a3776e5c50f15f9d
BoHan <han.bo1@husky.neu.edu>
2018-02-14 15:56:17 -0500

    ValidNumber.java

---
+++ ValidNumber.java
@@ -0,0 +1,58 @@
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        int i = 0;
+        int n = s.length();
+
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            i++;
+        }
+
+        boolean isNumeric = false;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            i++;
+            isNumeric = true;
+        }
+
+
+        if (i < n && s.charAt(i) == '.') {
+            i++;
+            while (i < n && Character.isDigit(s.charAt(i))) {
+                i++;
+                isNumeric = true;
+            }
+        }
+
+
+        if (isNumeric && i < n && (s.charAt(i) == 'e' || s.charAt(i) == 'E')) {
+            i++;
+            isNumeric = false;
+            if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+                i++;
+            }
+            while (i < n && Character.isDigit(s.charAt(i))) {
+                i++;
+                isNumeric = true;
+            }
+        }
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        return isNumeric && i == n;
+    }
+
+    public static void main(String[] args){
+        ValidNumber v = new ValidNumber();
+        String s = "-2e5";
+        System.out.println(v.isNumber(s));
+    }
+}
+
