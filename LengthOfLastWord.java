f67ed90d2141be03d878a6515f07be9877ffcb1a
BoHan <han.bo1@husky.neu.edu>
2018-02-14 16:54:09 -0500

    LengthOfLastWord.java-change

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,12 +1,25 @@
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-
+        /*
         String[] str = s.split(" ");
 
         if(str==null||str.length==0)
             return 0;
 
         return str[str.length-1].length();
+        */
+        if(s==null||s.length()==0)
+            return 0;
+
+        int size = s.length();
+        int count = 0;
+        for(int i=size-1;i>=0;i--){
+            if(s.charAt(i)!=' ')
+                count++;
+            if(s.charAt(i)==' '&&count!=0)
+                return count;
+        }
+        return count;
     }
 
     public static void main(String[] args){
